600290d515dd73de9c9b4ac6e6dbbac59feb9f48
Nilanchala Samal <nilanchala.samal00@gmail.com>
2024-05-16 20:42:40 +0530

    Customizing the Not-Found page

---
+++ routing-demo/src/app/products/[productId]/reviews/[reviewId]/not-found.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function NotFound() {
+  return (
+    <div className="flex justify-center items-center">
+      <h1>Review Not Found</h1>
+      <p>The Review Id must be less than 1000</p>
+    </div>
+  );
+}
+
+export default NotFound;
--- routing-demo/src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ routing-demo/src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,9 +1,15 @@
+import { notFound } from "next/navigation";
+
 const ReviewDetails = ({params} : {
     params : {
         productId: string,
         reviewId: string
     }
 }) => {
+
+  if(parseInt(params.reviewId) > 1000) {
+    notFound();
+  }
   return (
     <h1>
       Review {params.reviewId} for the Product with ProductId: {params.productId}
