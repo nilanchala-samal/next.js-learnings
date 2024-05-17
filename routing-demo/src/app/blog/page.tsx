8f4920c5c25f62e87d9b7c6b80de6c5c55722cab
Nilanchala Samal <nilanchala.samal00@gmail.com>
2024-05-17 18:12:33 +0530

    Title Metadata

--- routing-demo/src/app/blog/page.tsx
+++ routing-demo/src/app/blog/page.tsx
@@ -1,3 +1,13 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: {
+    absolute: "Blog",
+    default: "",
+    template: ""
+  }
+}
+
 const Blog = () => {
   return (
     <h1>
