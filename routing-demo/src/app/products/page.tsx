f40feb749a0292106b7f93b086630586e3095cfe
Nilanchala Samal <nilanchala.samal00@gmail.com>
2024-05-17 19:55:54 +0530

    Navigating through the Link component

--- routing-demo/src/app/products/page.tsx
+++ routing-demo/src/app/products/page.tsx
@@ -1,10 +1,16 @@
+import Link from "next/link";
+
 const ProductList = () => {
+  const productId = 100;
   return (
     <>
       <h1>ProductList</h1>
-      <h2>Product 1</h2>
-      <h2>Product 2</h2>
-      <h2>Product 3</h2>
+      <h2><Link href={"products/1"}>Product 1</Link></h2>
+      <h2><Link href={"products/2"}>Product 2</Link></h2>
+      <h2><Link href={"products/3"} replace={false}>Product 3</Link></h2>
+      {/* replace props of Link component replaces the current history state and no new entry will be created for the browser's history stack*/}
+      <h2><Link href={`products/${productId}`}>Product {productId}</Link></h2>
+      <Link href={"/"}>Click here to go to Home Page</Link>
     </>
   );
 };
