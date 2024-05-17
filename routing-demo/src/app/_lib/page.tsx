8eaec78ff533a8d87597e43ff1699340c5734e46
Nilanchala Samal <nilanchala.samal00@gmail.com>
2024-05-17 11:59:04 +0530

    Private Folders

---
+++ routing-demo/src/app/_lib/page.tsx
@@ -0,0 +1,3 @@
+export default function PrivateLibrary() {
+  return <h1>This is Private Library</h1>;
+}
---
+++ routing-demo/src/app/private%5Ffolder/page.tsx
@@ -0,0 +1,3 @@
+export default function PrivateFolder() {
+  return <h1>This is Private Folder page with %5F Prefix</h1>;
+}
